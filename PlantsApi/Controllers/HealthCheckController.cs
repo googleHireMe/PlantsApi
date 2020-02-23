377c85aa380f171a78192fd80a2d1a6be619e3db
niks <nanevokshonov@gmail.com>
2020-02-23 18:23:17 +0300

    HealthCheck + prod connection strings

---
+++ PlantsApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlantsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> GetAsync() => "App is online";
+    }
+}
