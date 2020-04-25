832443fd76e6098577cfbbdcb983c8bea22cc6e8
nnevokshonov <nnevokshonov@aligntech.com>
2020-04-25 17:21:00 +0300

    DevicePlant controller was added

---
+++ PlantsApi/Controllers/DevicePlantController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PlantsApi.Interfaces;
+using PlantsApi.Models;
+using PlantsApi.Models.Enums;
+using PlantsApi.Models.ViewModels;
+using PlantsApi.Repository;
+
+namespace PlantsApi.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class DevicePlantController : ControllerBase
+	{
+		private readonly IPlantsStateRepository plantsStateRepository;
+		private readonly IUsersRepository usersRepository;
+		private readonly UserManager<ApplicationUser> userManager;
+		private readonly SignInManager<ApplicationUser> signInManager;
+
+		public DevicePlantController(IPlantsStateRepository plantsStateRepository,
+									 IUsersRepository usersRepository,
+									 UserManager<ApplicationUser> userManager,
+									 SignInManager<ApplicationUser> signInManager)
+		{
+			this.userManager = userManager;
+			this.plantsStateRepository = plantsStateRepository;
+			this.usersRepository = usersRepository;
+			this.signInManager = signInManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> PostAsync([FromBody] DeviceInfoDto value)
+		{
+			var appUser = await userManager.FindByEmailAsync(value.UserEmail);
+			if (appUser != null)
+			{
+				await signInManager.SignOutAsync();
+				var signInResult = await signInManager
+					.PasswordSignInAsync(appUser, value.UserPassword, false, false);
+				if (signInResult.Succeeded) 
+				{
+					var user = usersRepository.GetUser(appUser.Id);
+					var plantState = DeviceInfoDto.MapToPlantState(value, user.Id);
+					var created = plantsStateRepository.CreatePlantState(plantState);
+					return Ok(); 
+				}
+			}
+			return Unauthorized();
+		}
+
+	}
+}
---
+++ PlantsApi/Models/ViewModels/DeviceInfoDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantsApi.Models.ViewModels
+{
+    public class DeviceInfoDto
+    {
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public int PlantId { get; set; }
+        public int Light { get; set; }
+        public int Temp { get; set; }
+        public int EnvHumid { get; set; }
+        public int SoilMoist { get; set; }
+        public int SoilEc { get; set; }
+
+        public static PlantState MapToPlantState(DeviceInfoDto dto, int userId)
+        {
+            return new PlantState
+            {
+                Time = DateTime.Now,
+                Light = dto.Light,
+                Temp = dto.Temp,
+                EnvHumid = dto.EnvHumid,
+                SoilMoist = dto.SoilMoist,
+                SoilEc = dto.SoilEc,
+                PlantId = dto.PlantId,
+                UserId = userId
+            };
+        }
+    }
+}
