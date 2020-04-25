ad9085c30c858c2777d83b7b6c9143f9e13b6cae
nnevokshonov <nnevokshonov@aligntech.com>
2020-04-25 16:17:57 +0300

    Runtime initializer added (empty)

---
+++ PlantsApi/Services/Initializers/RuntimeInitializer.cs
@@ -0,0 +1,47 @@
+﻿using PlantsApi.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantsApi.Services.Initializers
+{
+    public class RuntimeInitializer
+    {
+        private readonly IdentityContext identityContext;
+        private readonly PlantsContext plantsContext;
+        public RuntimeInitializer(IdentityContext identityContext,
+                                  PlantsContext plantsContext)
+        {
+            this.identityContext = identityContext;
+            this.plantsContext = plantsContext;
+        }
+
+        public void InitializePlants()
+        {
+
+        }
+
+        public void InitializePlantStates()
+        {
+
+        }
+
+        public void InitializeUsers()
+        {
+
+        }
+
+        public void InitializePlantAssignments()
+        {
+
+        }
+
+        public void InitializeIdentityUser()
+        {
+
+        }
+
+
+    }
+}
